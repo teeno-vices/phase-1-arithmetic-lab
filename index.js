30f10fa8b08692ffbd0c2b2825b9d09b91c55c76
Marc Ndegwa <macndegwa11296@gmail.com>
2024-03-23 19:09:02 +0300

    I have completed this lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random()) *10;
+const num3 = 10
+const num4 = 6
+const mod = num3 % num4;
+const max = Math.max(2,4,6,8,10,12,14,16,18,20);
+
+
 
 // Write your code here
